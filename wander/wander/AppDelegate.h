4aa3ea1fa572836d87e74a90e3626c565e21b54c
Ashley Sidoryk <ashleysidoryk4@gmail.com>
2020-03-07 13:14:43 -0500

    added wander files

---
+++ wander/wander/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  wander
+//
+//  Created by Ashley Sidoryk on 3/6/20.
+//  Copyright © 2020 Ashley Sidoryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ wander/wander/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wander
+//
+//  Created by Ashley Sidoryk on 3/6/20.
+//  Copyright © 2020 Ashley Sidoryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
